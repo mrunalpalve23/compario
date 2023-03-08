bdfa75a7d3167e7b673026715ceebf8b85446cde
HP <HP@Mrunal>
2023-03-08 14:22:03 +0530

    Creating new project

---
+++ compario/src/main/java/com/compario/ComparioApplication.java
@@ -0,0 +1,13 @@
+package com.compario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComparioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComparioApplication.class, args);
+	}
+
+}
